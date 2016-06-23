9395490f826fc4f9dc8140230ff5e8c1bf2bebad
wokamoto <wokamoto1973@gmail.com>
2016-06-23 12:58:09 +0900

    added comments

--- amimoto_update.sh
+++ amimoto_update.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash -x
+: update packages
 yum clean all
 yum -y update
 yum -y install --disablerepo=amzn-main --enablerepo=epel libwebp
@@ -6,6 +7,7 @@ yum -y install --disablerepo=amzn-main --enablerepo=epel libwebp
 hash jq || yum -y install jq
 hash git || yum -y install git
 
+: update chef-solo recipes
 [ ! -e /opt/local ] && mkdir -p /opt/local
 cd /opt/local
 [ -e /opt/local/chef-repo ] && rm -rf /opt/local/chef-repo
@@ -18,31 +20,36 @@ cd /opt/local
 cookbook_path ["/opt/local/chef-repo/cookbooks"]' > /opt/local/solo.rb
 [ ! -f /opt/local/amimoto.json ] && cp /opt/local/chef-repo/cookbooks/amimoto/amimoto.json /opt/local/amimoto.json
 
+: install chef-solo
 curl -L http://www.opscode.com/chef/install.sh | bash
 
 echo '#!/bin/bash
 /sbin/service monit stop
 [ -f /usr/bin/python2.7 ] && /usr/sbin/alternatives --set python /usr/bin/python2.7
+git -C /opt/local/chef-repo/cookbooks/amimoto pull origin 2016.01
 /opt/chef/bin/chef-solo -c /opt/local/solo.rb -j /opt/local/amimoto.json -l error' > /opt/local/provision
 if [ -f /opt/local/amimoto-managed.json ]; then
   sed -i -e 's/amimoto.json/amimoto-managed.json/g' /opt/local/provision
 fi
 chmod +x /opt/local/provision
 
+: remove old wp-cli
 rm -f /usr/bin/wp; rm -f /usr/local/bin/wp; rm -rf /usr/share/wp-cli/
 
+: update Nginx, PHP, MySQL...
 service monit stop; service php-fpm stop; service mysql stop
 yum remove -y php php54-* php55-* php56-* php-* Percona-* httpd*
 [ -f /usr/bin/python2.7 ] && /usr/sbin/alternatives --set python /usr/bin/python2.7
 if [ ! -f /opt/local/amimoto-managed.json ]; then
-  chef-solo -c /opt/local/solo.rb -j /opt/local/amimoto.json -l error
+  /opt/chef/bin/chef-solo -c /opt/local/solo.rb -j /opt/local/amimoto.json -l error
 else
-  chef-solo -c /opt/local/solo.rb -j /opt/local/amimoto-managed.json -l error
+  /opt/chef/bin/chef-solo -c /opt/local/solo.rb -j /opt/local/amimoto-managed.json -l error
 fi
 yum -y update
 /opt/local/provision
 
+: upgrade MySQL DB
 mysql_upgrade -u root
-curl -L -s http://bugs.mysql.com/file.php?id=19725\&bug_id=67179\&text=1 > innodb_stats_fix.sql
+#curl -L -s http://bugs.mysql.com/file.php?id=19725\&bug_id=67179\&text=1 > innodb_stats_fix.sql
 #mysql -u root mysql < innodb_stats_fix.sql
 service monit stop; service mysql restart; service monit start
