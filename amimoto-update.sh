063166b2b61ede58eb5ff4039c996a1c156d39e2
Daniel Olson <emaildano@gmail.com>
2018-01-25 12:47:17 -0500

    removed unused var

--- amimoto-update.sh
+++ amimoto-update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 TMP_JSON=$(mktemp)
-AMIMOTO_JSON='/opt/local/amimoto.json'
 git -C /opt/local/chef-repo/cookbooks/amimoto/ pull origin 2016.01
 jq '.phpfpm = "72"' /opt/local/amimoto.json > ${TMP_JSON}
 mv -f ${TMP_JSON} /opt/local/amimoto.json
