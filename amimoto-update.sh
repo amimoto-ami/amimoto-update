e89ad346350d7ff399a4afb8344950d0cd27e514
Daniel Olson <emaildano@gmail.com>
2018-01-25 11:59:30 -0500

    array update

--- amimoto-update.sh
+++ amimoto-update.sh
@@ -2,7 +2,6 @@
 
 TMP_JSON=$(mktemp)
 AMIMOTO_JSON=$(cat /opt/local/amimoto.json)
-
 git -C /opt/local/chef-repo/cookbooks/amimoto/ pull origin 2016.01
-jq -s '.[1] * .[0]' <(echo '{"phpfpm":{"version":"72"}}') <(echo $AMIMOTO_JSON) > /opt/local/amimoto.json
+jq -s '.[0] * .[1]' <(echo '{"phpfpm":{"version":"72"}}') <(echo $AMIMOTO_JSON) > /opt/local/amimoto.json
 mv -f ${TMP_JSON} /opt/local/amimoto.json
