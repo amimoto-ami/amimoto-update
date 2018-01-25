a7b54a75e49db3f658cab241d21bf5b29102a6d6
Daniel Olson <emaildano@gmail.com>
2018-01-25 12:48:51 -0500

    added provision script

--- amimoto-update.sh
+++ amimoto-update.sh
@@ -4,3 +4,5 @@ TMP_JSON=$(mktemp)
 git -C /opt/local/chef-repo/cookbooks/amimoto/ pull origin 2016.01
 jq '.phpfpm = "72"' /opt/local/amimoto.json > ${TMP_JSON}
 mv -f ${TMP_JSON} /opt/local/amimoto.json
+/opt/local/provision
+service php-fpm restart
